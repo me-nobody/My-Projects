ca5d00cac7a5f6d0d96ed6c2f8005ec1772f4d2a
anubratadas <anubratadas@gmail.com>
2019-01-27 01:20:29 +0530

    add file globbing

--- deira_CA.py
+++ deira_CA.py
@@ -6,25 +6,43 @@ Created on Sat Jan 26 15:34:12 2019
 @author: teetul
 """
 
+import glob
 import re
+
+def read_dir():
+    lst=[]
+    for file in glob.glob(r"/home/teetul/Documents/Deira_G4/*"):
+        lst.append(file)
+    return lst      
+
+
+
 def get_seqs():
-    with open(r"/home/teetul/Documents/Deira_G4/deiraDNA.fna","r") as fh:
+    seqlist=[]
+    filenames=read_dir()
+    for file in filenames:
+        fh=open(file,"r")    
         data=fh.read()
-    return data
+        seqlist.append(data)
+    print (seqlist)
+    
+get_seqs()    
 
 def pattern_compile():
     lst=[]
     patterns=["AAA[ATGC]{1,7}AAA[ATGC]{1,7}AAA[ATGC]{1,7}AAA","GGG[ATGC]{1,7}GGG[ATGC]{1,7}GGG[ATGC]{1,7}GGG"]
     for pattern in patterns:
          regex=re.compile(pattern)
-         
          lst.append(regex)
-    return lst, patterns
+    return patterns,lst
+    
 
 def pattern_search():
     dna=get_seqs()
-    pat_lst=pattern_compile()
-    for pattern in pat_lst:
-        print(pattern," length is ",len(re.findall(pattern,dna)))
+    pat_lst,pat_relist=pattern_compile()
+    count=0
+    for item in pat_relist:        
+        print(count," ",pat_lst[count]," length is ",len(re.findall(item,dna)))
+        count=count+1
 
-pattern_search()
+#pattern_search()
